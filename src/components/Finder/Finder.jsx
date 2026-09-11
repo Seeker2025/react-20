30921633c2d0a1d9bbf0e9961eb32bb258a8971c
Igor Yurovsky <85400723+Seeker2025@users.noreply.github.com>
2026-09-11 12:45:54 +0300

    fix
    Fixed an error in config.json.

---
+++ src/components/Finder/Finder.jsx
@@ -0,0 +1,14 @@
+import React, { Component} from "react"
+
+export class Finder extends Component{
+    state = {
+
+    };
+
+    render(){
+        return (
+            <p>Finder Component</p>
+        )
+    }
+    
+}
